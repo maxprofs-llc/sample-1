b23a9839b2673c118b49aef2aeb1cab93a3fc1a1
Joshua <pkjoshua123@gmail.com>
2018-02-02 11:24:23 +0530

    Nothing

---
+++ biodata.sh
@@ -0,0 +1,11 @@
+echo "Enter Name"
+read name
+echo "Enter Age"
+read Age
+echo "Enter Salary"
+read salary
+echo "USER PROFILE"
+echo "************"
+echo "Name Is "$name
+echo "Age Is "$Age
+echo "Salary Is "$salary
